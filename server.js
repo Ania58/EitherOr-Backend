98637c54c80acf9413b86f3b3da5c9ac6a390c3e
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-03-23 22:58:55 +0100

    require certain dependencies in server.js, start the server

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+const PORT = 3000;
+
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+})
