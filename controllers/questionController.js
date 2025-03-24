bf761dae7617a04a9da1e43ece21de73d36bf88b
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-03-25 00:20:44 +0100

    create three functions: createQuestion, getSpecificQuestion, and getRandomQuestion inside questionController.js

---
+++ controllers/questionController.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const Question = require("../models/Question");
+
+
+const createQuestion = async (req, res) => {
+    try {
+        const newQuestion = await Question.create({
+            optionOne: req.body.optionOne,
+            optionTwo: req.body.optionTwo,
+        });
+        res.status(201).json(newQuestion);
+    } catch (error) {
+        console.error("Error creating question" , error.message);
+        res.status(500).send("Error creating a question");
+    };
+};
+
+const getSpecificQuestion = async (req,res) => {
+    try {
+        const id = req.params.id;
+        const specificQuestion = await Question.findById(id);
+
+        res.status(200).json(specificQuestion);
+    } catch (error) {
+        console.error("Error finding requested question", error.message);
+        res.status(404).send("Question not found")
+    };
+};
+
+
+const getRandomQuestion = async (req, res) => {
+    try {
+        const count = await Question.countDocuments();
+        const randomIndex = Math.floor(Math.random() * count);
+        const randomQuestion = await Question.findOne().skip(randomIndex);
+
+        res.status(200).json(randomQuestion);
+    } catch (error) {
+        console.error("Error getting random question:", error.message);
+        res.status(500).send("Could not retrieve random question");
+    }
+};
+
+
+
+
+
+
+
+
+module.exports = { createQuestion, getSpecificQuestion, getRandomQuestion }
