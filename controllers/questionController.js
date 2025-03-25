c85020efea0dec6e9da2c0bbbc779c9981ff6d68
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-03-25 15:18:51 +0100

    create voteOnQuestion function and its post route, test it out with postman

--- controllers/questionController.js
+++ controllers/questionController.js
@@ -51,9 +51,35 @@ const getAllQuestions = async (req, res) => {
     }
 };
 
+const voteOnQuestion = async (req, res) => {
+    try {
+        const { option } = req.body;
+        const { id } = req.params;
+
+        if (!["optionOne", "optionTwo"].includes(option)) {
+            return res.status(400).send("Invalid option selected.");
+          };
+
+        const question = await Question.findById(id);
+
+        if (!question) {
+            return res.status(404).send("Question not found.");
+          };
+
+          const voteKey = option === "optionOne" ? "votesOptionOne" : "votesOptionTwo";
+          question[voteKey].push("anonymous");
+
+            await question.save();
+            res.status(200).json({ message: "Vote counted!", question });
+        
+    } catch (error) {
+        console.error("Error voting on question:", error.message);
+        res.status(500).send("Error voting on question.");
+    }
+};
 
 
 
 
 
-module.exports = { createQuestion, getSpecificQuestion, getRandomQuestion, getAllQuestions }
+module.exports = { createQuestion, getSpecificQuestion, getRandomQuestion, getAllQuestions, voteOnQuestion }
--- routes/questions.js
+++ routes/questions.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const { createQuestion, getSpecificQuestion, getRandomQuestion, getAllQuestions } = require('../controllers/questionController');
+const { createQuestion, getSpecificQuestion, getRandomQuestion, getAllQuestions, voteOnQuestion } = require('../controllers/questionController');
 
 
 router.post('/create', createQuestion);
 router.get('/random', getRandomQuestion);
 router.get('/:id', getSpecificQuestion);
+router.post('/:id/vote', voteOnQuestion);
 router.get('/', getAllQuestions);
 
 
