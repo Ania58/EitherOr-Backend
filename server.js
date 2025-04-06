27ed1557975e16edec2c78ca743301fbf938dd0c
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-04-06 18:04:43 +0200

    Use process.env.PORT for Render compatibility

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors());
